e9ebb44b307bd89a7fc22b2cd055210e9e86533d
ismoo14 <ismailabdiaman4@gmail.com>
2025-10-03 07:41:19 +0000

    initializing the backend part

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,32 @@
 import { Link } from "react-router-dom";
 import "./register.scss"
-
+import { useState } from "react";
+import axios from 'axios';
 const Register = () => {
+
+    const [inputs, setInputs] = useState({
+        username:"",
+        email:"",
+        password:"",
+        name:"",
+    });
+
+    const [err, setErr] = useState(false);
+
+    const handleChange =  (e) => {
+        setInputs((prev) => ({...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post("http://localhost:8800/api/auth/register", inputs);
+        } catch(err){
+        setErr(true);
+        }
+};
+    
 return (
     <div className="register">
         <div className="card">
@@ -18,15 +43,16 @@ return (
             <div className="right">
                 <h1>Register</h1>
                 <form>
-                    <input type="text" placeholder="Username"
+                    <input type="text" placeholder="Username" name="username" onChange={handleChange}
                     />
-                    <input type="email" placeholder="Email"
+                    <input type="email" placeholder="Email" name="email" onChange={handleChange}
                     />
-                    <input type="Password" placeholder="Password"
+                    <input type="Password" placeholder="Password" name="password" onChange={handleChange}
                     />
-                    <input type="text" placeholder="Name"
+                    <input type="text" placeholder="Name" name="name" onChange={handleChange}
                     />
-                    <button>Register</button>
+                    {err && ""}
+                    <button onClick={handleClick}>Register</button>
                 </form>
             </div>
         </div>
